408626513c8080c205a75c5817d7aa8d9cb64f59
Rerurate514 <rerurateyuto@gmail.com>
2026-03-23 14:56:54 +0900

    CurriculumSelectorの作成

---
+++ lib/presentation/pages/home/components/curriculum_selector.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tanni_simulator/application/usecase/loading_curriculum_usecase.dart';
+import 'package:tanni_simulator/domain/constants/curriculumn_type.dart';
+import 'package:tanni_simulator/l10n/app_localizations.dart';
+
+class CurriculumSelector extends StatefulHookConsumerWidget {
+  const CurriculumSelector({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CurriculumPulldownMenuState();
+}
+class _CurriculumPulldownMenuState extends ConsumerState<CurriculumSelector> {
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+    final selectedType = useState<CurriculumType?>(null);
+
+    return DropdownButtonFormField<CurriculumType>(
+      decoration: InputDecoration(
+        labelText: l10n.home_dropdown_button_hint,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      ),
+      initialValue: selectedType.value,
+      items: CurriculumType.values.map((type) {
+        return DropdownMenuItem(
+          value: type,
+          child: Text(type.label),
+        );
+      }).toList(),
+      onChanged: (CurriculumType? newType) {
+        if(newType == null) return;
+
+        selectedType.value = newType;
+
+        ref.read(loadingCurriculumUsecaseProvider.notifier).selectCurriculum(newType);
+      }
+    );
+  }
+}
