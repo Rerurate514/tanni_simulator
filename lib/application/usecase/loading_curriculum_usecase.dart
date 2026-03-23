247f3e4fe43ddf027e539509a52afb39a4ccf944
Rerurate514 <rerurateyuto@gmail.com>
2026-03-23 14:19:25 +0900

    カリキュラムを選んで返すenumとusecaseの追加

---
+++ lib/application/usecase/loading_curriculum_usecase.dart
@@ -0,0 +1,24 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:tanni_simulator/domain/constants/curriculumn_type.dart';
+import 'package:tanni_simulator/domain/entities/curriculum.dart';
+import 'package:tanni_simulator/infrastructure/repositories/yaml_repository.dart';
+
+part 'loading_curriculum_usecase.g.dart';
+
+@riverpod
+class LoadingCurriculumUsecase extends _$LoadingCurriculumUsecase {
+  @override
+  FutureOr<CurriculumModel?> build() async {
+    return null;
+  }
+
+  Future<void> selectCurriculum(CurriculumnType type) async {
+    state = AsyncLoading();
+
+    state = await AsyncValue.guard(() async {
+      final repo = ref.read(yamlRepositoryProvider);
+      final map = await repo.loadFromAssets(type.path);
+      return CurriculumModel.fromJson(map);
+    });
+  }
+}
---
+++ lib/domain/constants/curriculumn_type.dart
@@ -0,0 +1,11 @@
+enum CurriculumnType {
+  t2023("2023年度入学者, 情報通信工学科", "assets/data/curriculum/T-2023.yaml");
+
+  const CurriculumnType(
+    this.label,
+    this.path
+  );
+
+  final String label;
+  final String path;
+}
