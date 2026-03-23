c3ca8328f2fbac6b51fcac5d522b6b187155bc20
Rerurate514 <rerurateyuto@gmail.com>
2026-03-24 00:33:09 +0900

    isCompletedを実装

--- lib/domain/entities/course.dart
+++ lib/domain/entities/course.dart
@@ -11,6 +11,8 @@ sealed class CourseModel with _$CourseModel {
     @JsonKey(name: 'is_required') required bool isRequired,
     required int credits,
     required int term,
+    @Default(false)
+    bool isCompleted
   }) = _CourseModel;
 
   factory CourseModel.fromJson(Map<String, dynamic> json) => _$CourseModelFromJson(json);
