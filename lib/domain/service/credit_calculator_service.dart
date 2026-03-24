547f068c8aa1df82b4b3f235266176cbd7b9ecdf
Rerurate514 <rerurateyuto@gmail.com>
2026-03-25 00:53:32 +0900

    カテゴリごとの履修済み単位を返すメソッドの追加

--- lib/domain/service/credit_calculator_service.dart
+++ lib/domain/service/credit_calculator_service.dart
@@ -1,4 +1,5 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:tanni_simulator/domain/constants/category_type.dart';
 import 'package:tanni_simulator/domain/entities/course.dart';
 
 part 'credit_calculator_service.g.dart';
@@ -15,6 +16,13 @@ class CreditCalculatorService {
     return courses
     .where((course) => course.isCompleted)
     .toList();
+  }
+
+  int getEarnedCategoryCredits(List<CourseModel> courses, CategoryType categoryType) {
+    return courses
+      .where((course) => course.category == categoryType)
+      .where((course) => course.isCompleted)
+      .fold(0, (sum, course) => sum + course.credits);
   }
 
   int calculateTotal(List<CourseModel> courses) {
