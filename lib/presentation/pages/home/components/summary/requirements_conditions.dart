e49d8c5260693ff3411be37f3383c59ec92a5003
Rerurate514 <rerurateyuto@gmail.com>
2026-03-25 03:05:48 +0900

    Requirementの判定をproviderに分離

--- lib/presentation/pages/home/components/summary/requirements_conditions.dart
+++ lib/presentation/pages/home/components/summary/requirements_conditions.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:tanni_simulator/application/state/course_list_notifier.dart';
 import 'package:tanni_simulator/application/state/selected_requirement_notifier.dart';
-import 'package:tanni_simulator/core/utils/join_category_course.dart';
-import 'package:tanni_simulator/domain/constants/category_type.dart';
 import 'package:tanni_simulator/domain/entities/curriculum.dart';
-import 'package:tanni_simulator/domain/service/credit_calculator_service.dart';
-import 'package:tanni_simulator/domain/service/credit_requirements_condiction_service.dart';
 import 'package:tanni_simulator/presentation/pages/home/components/summary/check_requirement_status.dart';
 import 'package:tanni_simulator/presentation/pages/home/components/summary/is_requirement_met.dart';
 import 'package:tanni_simulator/presentation/pages/home/components/summary/missing_courses.dart';
+import 'package:tanni_simulator/presentation/pages/home/providers/requirement_conditions_providers.dart';
 import 'package:tanni_simulator/presentation/widgets/app_gap.dart';
 
 class RequirementsConditions extends HookConsumerWidget {
@@ -19,34 +15,15 @@ class RequirementsConditions extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ccService = ref.watch(creditCalculatorServiceProvider);
-    final crcService = ref.watch(creditRequirementsCondictionServiceProvider);
-
     final selectedRequirement = ref.watch(selectedRequirementProvider);
     if(selectedRequirement == null) return SizedBox.shrink();
 
-    final courses = ref.watch(courseListProvider);
-    final earned = ccService.getEarnedCredits(courses);
-
-    //最低単位数を満たしているかどうか
-    final isRequirementMet = crcService.isRequirementMet(
+    final isRequirementMet = ref.watch(isRequirementMetProvider(selectedRequirement));
+    final requirementStatus = ref.watch(requirementStatusProvider(selectedRequirement));
+    final missingCourses = ref.watch(missingCoursesProvider(
       selectedRequirement,
-      ccService.calculateTotal(courses)
-    );
-
-    //カテゴリごとの最低単位数を満たしているかどうか
-    final requirementStatus = crcService.checkRequirementStatus(
-      selectedRequirement,
-      ccService.getEarnedCategoryCredits(courses, CategoryType.professional),
-      ccService.getEarnedCategoryCredits(courses, CategoryType.general)
-    );
-
-    //進級に必要な必修科目を履修しているかどうか
-    final missingCourses = crcService.getMissingCourses(
-      selectedRequirement,
-      earned,
-      joinCategoryAllCourse(curriculum)
-    );
+      curriculum
+    ));
 
     return Card(
       margin: const EdgeInsets.all(16),
@@ -54,11 +31,17 @@ class RequirementsConditions extends HookConsumerWidget {
         padding: EdgeInsetsGeometry.all(8),
         child: Column(
           children: [
-            IsRequirementMet(isRequirementMet: isRequirementMet),
+            IsRequirementMet(
+              isRequirementMet: isRequirementMet
+            ),
             AppGap.s(),
-            CheckRequirementStatus(requirementStatus: requirementStatus),
+            CheckRequirementStatus(
+              requirementStatus: requirementStatus
+            ),
             AppGap.s(),
-            MissingCourses(missingCourses: missingCourses)
+            MissingCourses(
+              missingCourses: missingCourses
+            )
           ],
         ),
       )
---
+++ lib/presentation/pages/home/providers/requirement_conditions_providers.dart
@@ -0,0 +1,52 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:tanni_simulator/application/state/course_list_notifier.dart';
+import 'package:tanni_simulator/core/utils/join_category_course.dart';
+import 'package:tanni_simulator/domain/constants/category_type.dart';
+import 'package:tanni_simulator/domain/constants/requirement_status.dart';
+import 'package:tanni_simulator/domain/entities/course.dart';
+import 'package:tanni_simulator/domain/entities/curriculum.dart';
+import 'package:tanni_simulator/domain/entities/requirement.dart';
+import 'package:tanni_simulator/domain/service/credit_calculator_service.dart';
+import 'package:tanni_simulator/domain/service/credit_requirements_condiction_service.dart';
+
+part 'requirement_conditions_providers.g.dart';
+
+@riverpod
+bool isRequirementMet(Ref ref, RequirementModel selectedRequirement) {
+  final ccService = ref.watch(creditCalculatorServiceProvider);
+  final crcService = ref.watch(creditRequirementsCondictionServiceProvider);
+
+  final courses = ref.watch(courseListProvider);
+
+  return crcService.isRequirementMet(
+    selectedRequirement,
+    ccService.calculateTotal(courses)
+  );
+}
+
+@riverpod
+RequirementStatus requirementStatus(Ref ref, RequirementModel selectedRequirement) {
+  final ccService = ref.watch(creditCalculatorServiceProvider);
+  final crcService = ref.watch(creditRequirementsCondictionServiceProvider);
+  final courses = ref.watch(courseListProvider);
+
+  return crcService.checkRequirementStatus(
+    selectedRequirement,
+    ccService.getEarnedCategoryCredits(courses, CategoryType.professional),
+    ccService.getEarnedCategoryCredits(courses, CategoryType.general)
+  );
+}
+
+@riverpod
+List<CourseModel> missingCourses(Ref ref, RequirementModel selectedRequirement, CurriculumModel curriculum) {
+  final ccService = ref.watch(creditCalculatorServiceProvider);
+  final crcService = ref.watch(creditRequirementsCondictionServiceProvider);
+  final courses = ref.watch(courseListProvider);
+  final earned = ccService.getEarnedCredits(courses);
+  
+  return crcService.getMissingCourses(
+    selectedRequirement,
+    earned,
+    joinCategoryAllCourse(curriculum)
+  );
+}
