ffe679ccbeab7f52d3856e009808656cf11aa663
Rerurate514 <rerurateyuto@gmail.com>
2026-03-26 03:47:05 +0900

    色を追加

--- lib/core/utils/app_color_scheme.dart
+++ lib/core/utils/app_color_scheme.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 extension CustomColorScheme on ColorScheme {
   Color get success => const Color(0xFF28A745);
+  Color get exclusive => const Color(0xFF6F42C1);
 }
