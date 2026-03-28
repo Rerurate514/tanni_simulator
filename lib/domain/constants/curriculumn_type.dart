1a266241b95cd8fe00fd5043785a22df21d8ac11
Rerurate514 <rerurateyuto@gmail.com>
2026-03-28 11:50:36 +0900

    2026年度版追加

--- lib/domain/constants/curriculumn_type.dart
+++ lib/domain/constants/curriculumn_type.dart
@@ -1,5 +1,8 @@
 enum CurriculumType {
-  t2023('2023年度入学者, 情報通信工学科', 'assets/data/curriculum/T-2023.yaml');
+  t2023('2023年度入学者, 情報通信工学科', 'assets/data/curriculum/T-2023.yaml'),
+  t2024('2024年度入学者, 情報通信工学科', 'assets/data/curriculum/T-2024.yaml'),
+  t2025('2025年度入学者, 情報通信工学科', 'assets/data/curriculum/T-2025.yaml'),
+  t2026('2026年度入学者, 情報通信工学科', 'assets/data/curriculum/T-2026.yaml');
 
   const CurriculumType(
     this.label,
